46bbe9e51ec4783d1a7dd07717f948496a78351d
Eric <emartinez@greenriver.com.mx>
2016-10-04 21:50:15 -0500

    Add project files.

---
+++ AntBoxRepots/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AntBoxRepots.Startup))]
+namespace AntBoxRepots
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
